7ff5c70ada057a67c10424ad8493432122f60051
splitlang <seanmark.student@moringaschool>
2024-05-22 10:14:02 +0300

    completed lab

---
+++ lib/app.py
@@ -0,0 +1,7 @@
+print("Hello World!Pass his test,please.")
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
